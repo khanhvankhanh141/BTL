a8e5cb79320ff36a32f9384dd7f1fd452a2ca493
ĐOÀN VIỆT KHÁNH <khanhvankhanh141@gmail.com>
2022-12-26 00:51:44 +0700

    first

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//CONNECT DB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
