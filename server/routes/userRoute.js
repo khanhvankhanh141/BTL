b5380daa01bdaa52f877577c43b0ebc24d7703cf
ĐOÀN VIỆT KHÁNH <khanhvankhanh141@gmail.com>
2022-12-28 23:55:58 +0700

    user-controller

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getUser,
+} = require("../controllers/userController");
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/getuser", getUser);
 
 module.exports = router;
