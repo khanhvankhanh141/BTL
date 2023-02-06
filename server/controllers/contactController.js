ab33623c2bd90ad0bbbe51a3caee0993f3edf241
ĐOÀN VIỆT KHÁNH <khanhvankhanh141@gmail.com>
2023-02-06 23:28:37 +0700

    update

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,7 +1,21 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
 const contactUs = asyncHandler(async (req, res) => {
-  res.send("Contact");
+  const { subject, message } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+  if (!user) {
+    res.status(400);
+    throw new Error("User not found, Please signup");
+  }
+
+  // Validation
+  if (!subject || !message) {
+    res.status(400);
+    throw new Error("Please add subject and message");
+  }
 });
 
 module.exports = {
--- server/routes/contactRoute.js
+++ server/routes/contactRoute.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const { contactUs } = require("../controllers/contactController");
 const protected = require("../middleWare/authMiddleware");
 
+router.post("/", protected, contactUs);
+
 module.exports = router;
